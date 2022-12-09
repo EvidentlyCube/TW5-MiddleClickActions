a8113a45fe023df0fbe6114b06778dcae9a5e6b2
Maurycy Zarzycki <maurycy@evidentlycube.com>
2022-12-09 18:25:16 +0100

    + Added `name` to plugin.info + Fixed indent character from spaces to tabs

--- plugin/tiddlers/middle-click-actions.js
+++ plugin/tiddlers/middle-click-actions.js
@@ -8,44 +8,44 @@ module-type: widget
 
 (function() {
 
-    /*jslint node: true, browser: true */
-    /*global $tw: false */
-    "use strict";
+	/*jslint node: true, browser: true */
+	/*global $tw: false */
+	"use strict";
 
-    var Widget = require("$:/core/modules/widgets/widget.js").widget;
+	var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
-    var MiddleClickActionsWidget = function(parseTreeNode,options) {
-        this.initialise(parseTreeNode,options);
-    };
+	var MiddleClickActionsWidget = function(parseTreeNode,options) {
+		this.initialise(parseTreeNode,options);
+	};
 
-    /*
-    Inherit from the base widget class
-    */
-    MiddleClickActionsWidget.prototype = new Widget();
+	/*
+	Inherit from the base widget class
+	*/
+	MiddleClickActionsWidget.prototype = new Widget();
 
 
-    /*
-    Render this widget into the DOM
-    */
-    MiddleClickActionsWidget.prototype.render = function(parent,nextSibling) {
-        var self = this;
-        this.parentDomNode = parent;
-        this.computeAttributes();
-        this.execute();
-        parent.addEventListener("mousedown",function (event) {
-            if (event.button === 1) {
-                event.preventDefault();
-                event.stopPropagation();
-                self.invokeActions(self, event);
-            }
-        },false)
-        this.renderChildren(parent,nextSibling);
-    };
+	/*
+	Render this widget into the DOM
+	*/
+	MiddleClickActionsWidget.prototype.render = function(parent,nextSibling) {
+		var self = this;
+		this.parentDomNode = parent;
+		this.computeAttributes();
+		this.execute();
+		parent.addEventListener("mousedown",function (event) {
+			if (event.button === 1) {
+				event.preventDefault();
+				event.stopPropagation();
+				self.invokeActions(self, event);
+			}
+		},false)
+		this.renderChildren(parent,nextSibling);
+	};
 
-    MiddleClickActionsWidget.prototype.allowActionPropagation = function() {
-        return false;
-    };
+	MiddleClickActionsWidget.prototype.allowActionPropagation = function() {
+		return false;
+	};
 
-    exports["middle-click-actions"] = MiddleClickActionsWidget;
+	exports["middle-click-actions"] = MiddleClickActionsWidget;
 
 })();
